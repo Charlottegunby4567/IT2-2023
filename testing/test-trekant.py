998f9860ed7017b592a3fc647fab168da7a27730
Charlottegunby4567 <chabgun@online.no>
2024-03-05 11:03:34 +0100

    Oppdatering

---
+++ testing/test-trekant.py
@@ -0,0 +1,5 @@
+import unittest
+
+class TestTrekant(unittest.TestCase):
+    def setUp(self):
+        
---
+++ testing/trekant.py
@@ -0,0 +1,17 @@
+
+class Trekant:
+    def __init__(self, grunnlinje: int, høyde: int) -> None:
+        self.grunnlinje: int = grunnlinje
+        self.høyde: int = høyde
+
+    def areal(self):
+        return (self.grunnlinje * self.høyde) /2
+
+
+if __name__ == "__main__":
+    print("Testing!")
+
+    min_trekant = Trekant(10,10)
+    min_trekant.areal()
+
+
